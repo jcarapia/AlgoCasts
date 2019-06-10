e8b311215a50ddabd5890e4d1b4d52daeaf55b45
Jorge Carapia <jorge.carapia@candid.org>
2019-06-09 21:00:08 -0700

    reverse

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,36 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// -- Traditional for loop -- 
+// function reverse(str) {
+// 	let reverse = '';
+
+// 	for(let i = str.length-1; i >= 0; i--){
+// 		reverse += str[i];
+// 	}
+
+// 	return reverse;
+// }
+
+// -- es6 for loop 
+// function reverse(str) {
+// 	let reverse = '';
+
+// 	for(let char of str){
+// 		reverse = char + reverse;
+// 	}
+
+// 	return reverse;
+// }
+
+// -- using reduce
+function reverse(str) {
+	return str.split('').reduce((accum, char) =>{
+		return char + accum;
+	}, '')
+}
+
+//not included -- str.split('').reverse().join('');
 
 module.exports = reverse;
+
