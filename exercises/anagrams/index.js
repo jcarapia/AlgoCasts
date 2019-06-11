9c492f945f335b5d9c9d8f303366af91d31fb545
Jorge Carapia <jorge.carapia@candid.org>
2019-06-10 21:30:09 -0700

    'anagrams'

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,38 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+//solution using array.sort()
+function anagrams(stringA, stringB){
+	return cleanString(stringA) === cleanString(stringB);
+}
+
+function cleanString(string){
+	return string.replace(/[^A-Z0-9]/ig, "").split('').sort().join('').toLowerCase();
+}
+
+//solution using character maps
+// function anagrams(stringA, stringB) {
+// 	const strA = stringA.replace(/[^A-Z0-9]/ig, "").toLowerCase();
+// 	const strB = stringB.replace(/[^A-Z0-9]/ig, "").toLowerCase();
+
+// 	if(strA.length !== strB.length) return false;
+
+// 	let charMap = {};
+
+// 	for(char of strA){
+// 		charMap[char] = charMap[char] + 1 || 1;
+// 	}
+
+// 	for(char of strB){
+// 		if(!charMap[char]){
+// 			return false;
+// 		}
+// 		charMap[char]--
+// 	}
+
+// 	return true;
+
+// }
 
 module.exports = anagrams;
+
